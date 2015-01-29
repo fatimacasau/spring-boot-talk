67460a5d7f61be2ac8a9823ff100f81705fadba6
Fatima Casau <fcasau@paradigmatecnologico.com>
2015-01-29 23:43:00 +0100

    location rest service in groovy

--- src/main/groovy/com/paradigma/springboot/controllers/LocationController.groovy
+++ src/main/groovy/com/paradigma/springboot/controllers/LocationController.groovy
@@ -1,7 +1,63 @@
 package com.paradigma.springboot.controllers
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.RestTemplate
+
 /**
  * Created by fcasau on 1/29/15.
  */
-class LocationController {
+@RestController
+@ConfigurationProperties(prefix = "api")
+public class LocationController {
+
+    String url
+
+    String uri
+
+    String param
+
+    String defaultValue
+
+    @RequestMapping(value = "/location", method = RequestMethod.GET)
+    @ResponseBody
+    def findCityLocalization(
+            @RequestParam(value = "city", required = false) String city
+    ) {
+        city = city ?: defaultValue
+        new RestTemplate().getForObject("$url$uri?$param=$city", Map.class)
+    }
+
+    public String getUrl() {
+        url
+    }
+
+    public void setUrl(String url) {
+        this.url = url
+    }
+
+    public String getUri() {
+        uri
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri
+    }
+
+    public String getParam() {
+        param
+    }
+
+    public void setParam(String param) {
+        this.param = param
+    }
+
+    public String getDefaultValue() {
+        defaultValue
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue
+    }
+
 }
